4c381580232912617d9bb48c4a6295fbb3da1ace
Paul Dean <hello@pauldean.me>
2020-10-26 13:57:08 +0100

    more pages and styles

--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
+const staticCacheName = 'static-v1';
+const dynamicCacheName = 'dynamic-v1';
+
+const cacheNames = [
+    staticCacheName,
+    dynamicCacheName
+];
+
+const staticAssets = [
+    
+]
+
 self.addEventListener('install', (e)=>{
     console.log("SW installed")
+
+    e.waitUntil(
+        caches.open(staticCacheName).then(static =>{
+            static.addAll(staticAssets)
+        })
+    )
 })
 
 self.addEventListener('activate', (e)=>{
