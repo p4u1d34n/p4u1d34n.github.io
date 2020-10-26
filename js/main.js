914f1ccbdb76266ca7a830a42837e9c6c2dc967d
Paul Dean <hello@pauldean.me>
2020-10-26 12:03:34 +0100

    main

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 // SW INIT BEGIN
-if('serviceWorker' in navigator) {
+
+if ('serviceWorker' in navigator) {
+
     console.log("SW supported");
-    window.addEventListener('load',function(){
-        navigator.serviceWorker.register('sw_cache_pages.js').then( reg => {
-            console.log("SW Registered")
-        }).catch(e => {
-            console.error(e);
-        })
-    })
+
+    // Register a service worker hosted at the root of the
+    // site using the default scope.
+    navigator.serviceWorker.register('/sw_cache_pages.js').then(function (registration) {
+        console.log('Service worker registration succeeded:', registration);
+    }, /*catch*/ function (error) {
+        console.log('Service worker registration failed:', error);
+    });
+} else {
+    console.log('Service workers are not supported.');
 }
-// SW INIT END
+
+
