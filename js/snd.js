e0b496c87280981bf89be2b1f7bc1d533b07eb35
Paul Dean <hello@pauldean.me>
2020-10-27 00:08:19 +0100

    dns

--- js/snd.js
+++ js/snd.js
@@ -12,6 +12,10 @@ navigator.mediaDevices.getUserMedia(
   err => console.log(err)
 );
 
+analyser.fftSize = 2048;
+const bufferLength = analyser.fftSize;
+const dataArray = new Uint8Array(bufferLength);
+
 const dataArray = new Uint8Array(analyser.frequencyBinCount);
 var canvas = document.getElementById('canvas');
 const canvasCtx = canvas.getContext('2d');
@@ -37,6 +41,21 @@ const drawWave = () => {
   }); */
 
   analyser.getByteTimeDomainData(dataArray);
+
+  let lastPos = 0;
+  dataArray.forEach((item, i) => {
+    if (item > 128 && lastItem <= 128) { // we have crossed below the mid point
+      const elapsedSteps = i - lastPos; // how far since the last time we did this
+      lastPos = i;
+
+      const hertz = 1 / (elapsedSteps / 44100);
+      pitchSamples.push(hertz); // an array of every pitch encountered
+    }
+
+    canvasContext.fillRect(i, item, 1, 1); // point in the wave
+
+    lastItem = item;
+  });
 
   canvasCtx.fillStyle = 'rgb(200, 200, 200)';
   canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
