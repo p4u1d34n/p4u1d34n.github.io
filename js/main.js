10702282e725d286d3cc8ec632032f3fa2256ec4
Paul Dean <hello@pauldean.me>
2020-10-26 12:05:19 +0100

    main

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ if ('serviceWorker' in navigator) {
 
     // Register a service worker hosted at the root of the
     // site using the default scope.
-    navigator.serviceWorker.register('/sw_cache_pages.js').then(function (registration) {
+    navigator.serviceWorker.register('/sw_cache_pages.js', {scope: './'}).then(function (registration) {
         console.log('Service worker registration succeeded:', registration);
     }, /*catch*/ function (error) {
         console.log('Service worker registration failed:', error);
