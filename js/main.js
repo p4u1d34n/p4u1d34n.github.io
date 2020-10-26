8982d080765b5318987aa0427e6f774a0615c27b
Paul Dean <hello@pauldean.me>
2020-10-26 12:12:44 +0100

    main sw cahnnges

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
     console.log("SW supported");
     // Register a service worker hosted at the root of the
     // site using the default scope.
-    navigator.serviceWorker.register('./sw_cache_pages.js', {scope: './'}).then(function (registration) {
+    navigator.serviceWorker.register('../sw.js').then(function (registration) {
         console.log('Service worker registration succeeded:', registration);
     }, /*catch*/ function (error) {
         console.log('Service worker registration failed:', error);
