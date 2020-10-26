6e013b5650cffccaef04203573ff9870e31d931a
Paul Dean <hello@pauldean.me>
2020-10-26 11:38:14 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator) {
     console.log("SW supported");
     window.addEventListener('load',()=>{
-        navigator.serviceWorker.register('/sw_cache_pages.js').then( reg => {
+        navigator.serviceWorker.register('sw_cache_pages.js').then( reg => {
             console.log("SW Registered")
         }).catch(e => {
             console.error(e);
