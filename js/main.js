3c92b0261b35bfb855729b183372e0ab1cf02e5e
Paul Dean <hello@pauldean.me>
2020-10-26 12:13:31 +0100

    main

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 if ('serviceWorker' in navigator) {
     console.log("SW supported");
     // Register a service worker hosted at the root of the
-    // site using the default scope.
     navigator.serviceWorker.register('../sw.js').then(function (registration) {
         console.log('Service worker registration succeeded:', registration);
     }, /*catch*/ function (error) {
