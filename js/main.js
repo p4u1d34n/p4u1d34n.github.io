6e936b462e6c93077f6efdd8f4c1c6003a887ea3
Paul Dean <hello@pauldean.me>
2020-10-26 12:07:43 +0100

    main

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 // SW INIT BEGIN
-
 if ('serviceWorker' in navigator) {
-
     console.log("SW supported");
-
     // Register a service worker hosted at the root of the
     // site using the default scope.
     navigator.serviceWorker.register('./sw_cache_pages.js', {scope: './'}).then(function (registration) {
@@ -14,5 +11,4 @@ if ('serviceWorker' in navigator) {
 } else {
     console.log('Service workers are not supported.');
 }
-
-
+// SW INIT ENDED
